7bfecb0c36b93399af9f048d1fd564eda631e753
ALLLLLL703 <vapev433@qq.com>
2025-06-06 03:20:59 -0400

    day2

--- lua/config/keymap/lspsaga_key.lua
+++ lua/config/keymap/lspsaga_key.lua
@@ -2,6 +2,6 @@ vim.keymap.set("n", "glp", ":Lspsaga peek_definition<CR>", { silent = true, nore
 vim.keymap.set("n", "glr", ":Lspsaga finder<CR>", { silent = true, noremap = true, desc = "Peek reference" })
 vim.keymap.set("n", "<A-x>", "<cmd>Lspsaga term_toggle<CR>")
 vim.keymap.set("t", "<A-x>", "<cmd>Lspsaga term_toggle<CR>")
-vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true, noremap = true, desc = "Hover Doc" })
+-- vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", { silent = true, noremap = true, desc = "Hover Doc" })
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "tree_explorer" })
 -- 毎日、私はご飯を食べます。
