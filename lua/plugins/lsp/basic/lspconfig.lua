bda84146bbad48c36cc80b0151d0ca1e8a7a701a
ALLLLLL703 <vapev433@qq.com>
2025-07-01 08:02:19 -0400

    jdtls

--- lua/plugins/lsp/basic/lspconfig.lua
+++ lua/plugins/lsp/basic/lspconfig.lua
@@ -29,6 +29,18 @@ return {
 			vim.lsp.enable("bash")
 			vim.lsp.enable("asm_lsp")
 			vim.lsp.enable("pyright")
+			-- vim.lsp.enable("jdtls")
+		end,
+	},
+	{
+		"mfussenegger/nvim-jdtls",
+		event = "VeryLazy",
+		config = function()
+			local config = {
+				cmd = { "jdtls" },
+				root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+			}
+			require("jdtls").start_or_attach(config)
 		end,
 	},
 }
