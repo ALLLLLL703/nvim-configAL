effa53db96ef82a5be8efd6d97d6d7f8bb63d788
ALLLLLL703 <vapev433@qq.com>
2025-07-03 00:03:25 -0400

    little change to alpha

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -20,4 +20,22 @@ return {
 			require("keymaps.editor")
 		end,
 	},
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1001, -- this plugin needs to run before anything else
+		opts = {
+			rocks = { "magick" },
+		},
+	},
+	{
+		"3rd/image.nvim",
+		dependencies = { "luarocks.nvim" },
+		config = function()
+			require("image").setup({
+				backend = "ueberzug",
+				max_height_window_percentage = 50,
+				hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
+			})
+		end,
+	},
 }
--- lua/plugins/lsp/basic/lspconfig.lua
+++ lua/plugins/lsp/basic/lspconfig.lua
@@ -29,18 +29,10 @@ return {
 			vim.lsp.enable("bash")
 			vim.lsp.enable("asm_lsp")
 			vim.lsp.enable("pyright")
-			-- vim.lsp.enable("jdtls")
+			require("lspconfig").jdtls.setup({})
 		end,
 	},
 	{
 		"mfussenegger/nvim-jdtls",
-		event = "VeryLazy",
-		config = function()
-			local config = {
-				cmd = { "jdtls" },
-				root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-			}
-			require("jdtls").start_or_attach(config)
-		end,
 	},
 }
