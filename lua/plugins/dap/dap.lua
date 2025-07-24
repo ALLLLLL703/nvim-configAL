164cbf1f6df4776c27d8ab7f3a8b165048d28c5b
ALLLLLL703 <vapev433@qq.com>
2025-07-24 09:30:27 -0400

    dap2

---
+++ lua/plugins/dap/dap.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		event = "VeryLazy",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+			"jay-babu/mason-nvim-dap.nvim",
+			"theHamsta/nvim-dap-virtual-text",
+		},
+		config = function()
+			require("config.plugins.dap.dap")
+			require("keymaps.run_and_debug.code_runner")
+		end,
+	},
+}
